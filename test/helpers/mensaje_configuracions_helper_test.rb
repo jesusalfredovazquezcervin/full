4129d9bc5cac636732a53176817fdc0fc1897245
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-10-02 21:16:27 -0500

    -se agregan los archivos test para la pantalla de conf de captu de mensajes.

---
+++ test/helpers/mensaje_configuracions_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class MensajeConfiguracionsHelperTest < ActionView::TestCase
+end
---
+++ test/models/mensaje_configuracion_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MensajeConfiguracionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
