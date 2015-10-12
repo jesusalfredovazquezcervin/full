200feaff1be7140615bf95d4f44f1e5befee79a5
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-10-11 23:16:44 -0500

    se configura el idioma de la app en ES

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Myapp
 
     config.assets.precompile += [ 'appviews.css', 'cssanimations.css', 'dashboards.css', 'forms.css', 'gallery.css', 'graphs.css', 'mailbox.css', 'miscellaneous.css', 'pages.css', 'tables.css', 'uielements.css', 'widgets.css' ]
     config.assets.precompile += [ 'appviews.js', 'cssanimations.js', 'dashboards.js', 'forms.js', 'gallery.js', 'graphs.js', 'mailbox.js', 'miscellaneous.js', 'pages.js', 'tables.js', 'uielements.js', 'widgets.js', ]
+    config.i18n.default_locale = :es
   end
 end
