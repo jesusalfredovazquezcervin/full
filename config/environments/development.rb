b32b33bcf749288d70b6be233c87a41d9428a5ef
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-10-16 10:36:26 -0500

    --se modifican ambos archivos con la configuracion de envio de correos a traves de gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,18 @@ Myapp::Application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin"
 
   #Configuracion para el envio de correos de DEVISE
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options[:host]
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: 'gmail.com',
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: 'ofintelcal@gmail.com',
+      password: 'Abcd_1234'
+  }
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,17 @@ Myapp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.default_url_options = { host: '104.236.154.71', port: 3000 }
-
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: 'gmail.com',
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: 'ofintelcal@gmail.com',
+      password: 'Abcd_1234'
+  }
 end
