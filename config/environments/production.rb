387a4aa3c3555ef889c9715ec5f87de70153d578
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-10-16 11:15:53 -0500

    Se modifica la linea del host, se le quita el puerto 3000, porque en produccion no se usa el pto 3000

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Myapp::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.default_url_options = { host: '104.236.154.71', port: 3000 }
+  config.action_mailer.default_url_options = { host: '104.236.154.71' }
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
