942597d94960e2f1580304976fe8c70a6a82c0d4
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-10-27 14:54:00 -0600

    --se agrega el hasone para los datos generales , esto hacia que no se pudiera usar bin el modelo en el topnavbar y tronara en produccion

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,6 +1,7 @@
 class Cliente < ActiveRecord::Base
   has_one :direccion
   has_one :formapago
+  has_one :datosgenerale
   validates :nombre,
             presence: true,
             uniqueness: {scope: :rfc}
