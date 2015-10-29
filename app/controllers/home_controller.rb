19f5d9501192212b8f24fd4385401c9e68cf2981
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-10-29 10:38:14 -0600

    --se agrega la abilidad

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,21 @@
 class HomeController < ApplicationController
   def index
-    dashboard_4
+    if usuario_signed_in?
+      role = current_user.role
+      case role # was case obj.class
+        when 'Admin'
+          dashboard_4
+        when 'Operador'
+          respond_to do |format|
+            format.html { redirect_to captures_path}
+            format.json { head :no_content }
+          end
+        else # Consulta
+          dashboard_4
+      end
+    else
+      dashboard_4
+    end
   end
   def login
     render :layout => "empty"
