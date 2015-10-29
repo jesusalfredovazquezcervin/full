0a629792e0bba0c12683ca1798de2b9c78a2eba5
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-10-29 13:32:19 -0600

    --se agrega has_one : mensaje configuracion para que se pueda tener acceso a la configuracion de mensajes desde el cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,6 +2,7 @@ class Cliente < ActiveRecord::Base
   #has_one :direccion
   has_one :formapago
   has_one :datosgenerale
+  has_one :mensaje_configuracion
   validates :nombre,
             presence: true,
             uniqueness: {scope: :rfc}
