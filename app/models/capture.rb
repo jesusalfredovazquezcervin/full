8868f94ae188eeb4e17ea789202b8f762280f664
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-11-10 16:20:59 -0600

    --se agregan algunas listas para la captura de mensajes, de acuerdo a lo especificado por S.Vazquez

--- app/models/capture.rb
+++ app/models/capture.rb
@@ -2,4 +2,12 @@ class Capture < ActiveRecord::Base
   belongs_to :cliente
   validates :cliente_id,
             presence: true
+
+  TIPOLLAMADA = %w[tipo1 tipo2 tipo3] #tipoLlamada - Pendiente lista de valores
+  PAIS = %w[tipo1 tipo2 tipo3] #paisOrigino -  Pendiente lista de valores
+  FILIACION = %w[tipo1 tipo2 tipo3] #relacionPaciente -  Pendiente lista de valores
+  INTENSIDADSENAL = %w[tipo1 tipo2 tipo3] #intensidadSenal -  Pendiente lista de valores
+  DISPOSITIVO = %w[pc-escritorio laptop tablet movil] #dispositivo
+  FORMAPAGO = %w[efectivo tarjeta cheque transferencia deposito] #formaPagoFianza
+
 end
