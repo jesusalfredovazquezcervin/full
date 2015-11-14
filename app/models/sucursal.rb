51d411a365c0c2d7c136f50224fe67f8be1be853
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-11-14 11:00:57 -0600

    --se agrega relacion de horario y dereccion para mejorar el funcionamiento

--- app/models/sucursal.rb
+++ app/models/sucursal.rb
@@ -1,6 +1,8 @@
 class Sucursal < ActiveRecord::Base
   belongs_to :cliente
   has_one :contacto
+  has_one :direccion
+  has_one :horario
   validates :cliente_id, :nombre,
             presence: true
   validates :nombre,
