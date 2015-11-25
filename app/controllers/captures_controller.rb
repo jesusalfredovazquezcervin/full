dca4a0325e890fe4a3be7bcc2f3a5f514c8d9e17
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-11-25 13:11:47 -0600

    --se agrega en el index y consultar el notificactions new para ue funcione la forma de agregar notificacion  --se agrega @contactos que sirven para el directorio

--- app/controllers/captures_controller.rb
+++ app/controllers/captures_controller.rb
@@ -11,7 +11,9 @@ class CapturesController < ApplicationController
       @cliente = Cliente.find(params[:id])
     end
     @captures = Capture.all
+    @notification = Notification.new
     dashofintel
+
   end
 
   # GET /captures/1
@@ -30,6 +32,7 @@ class CapturesController < ApplicationController
 
   # GET /captures/1/consultar
   def consultar
+    @notification = Notification.new
     @captures = Capture.where(:cliente_id => params[:capture][:cliente_id])
     @cliente = Cliente.find(params[:capture][:cliente_id])
     @capture= Capture.new
