1010d76c7a8614a56cb8c4f8f674fd148f2c8017
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2015-12-02 10:44:09 -0600

    --se agrega la relacion a los nuevos campos persona y recipiente del tipo contacto  y se elimina la relacion a contacto --se agrega el scope active que sirve para encontrar las notificaciones activas del cliente

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,13 @@ class Notification < ActiveRecord::Base
   belongs_to :cliente
   belongs_to :sucursal
   belongs_to :usuario
-  belongs_to :contacto
   belongs_to :status
   belongs_to :event
+
+  belongs_to :person, :class_name => "Contacto"
+  belongs_to :recipient, :class_name => 'Contacto'
+
+  scope :active, -> (cliente) {
+    where(:cliente_id => 2).select{|m| m if (m.duedatetime.utc + (DateTime.now.utc_offset*(-1).second)  > DateTime.now.utc) }
+  }
 end
