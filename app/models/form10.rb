a4bfdb49478e4ea4573e5361b71f90ffae955146
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-01-22 12:42:24 -0600

    -- faltaban por subir los modelos de los formularios 9 y 10

---
+++ app/models/form10.rb
@@ -0,0 +1,4 @@
+class Form10 < ActiveRecord::Base
+  belongs_to :cliente
+  belongs_to :usuario
+end
---
+++ app/models/form9.rb
@@ -0,0 +1,4 @@
+class Form9 < ActiveRecord::Base
+  belongs_to :cliente
+  belongs_to :usuario
+end
