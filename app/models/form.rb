e8db3a889450f9d6c238560f927772a18f4d7b71
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-02-05 11:39:14 -0600

    --se agregan validaciones

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,4 +1,9 @@
 class Form < ActiveRecord::Base
   has_many :fields
   belongs_to :cliente
+  validates :cliente_id, :name,
+            presence: true
+  validates :cliente_id,
+            uniqueness:  {scope: [:name]}
+
 end
