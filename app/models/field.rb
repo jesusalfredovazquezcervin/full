fbd81b18f56125fbc2c55bb53c9f67f80625af18
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-02-05 11:40:35 -0600

    --se agregan validaciones

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,4 +1,10 @@
 class Field < ActiveRecord::Base
   belongs_to :form, touch: true
   DATA_TYPE = %w[String Integer Date DateTime]
+
+  validates :form_id, :name,
+            presence: true
+  validates :form_id,
+            uniqueness:  {scope: [:name]}
+
 end
