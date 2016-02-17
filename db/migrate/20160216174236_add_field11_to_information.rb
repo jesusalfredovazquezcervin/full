73060fa37eaeaea3f71d57cc1a0ae16ea0d4d8cd
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-02-17 16:01:01 -0600

    se suben el cambio que agrega campos que faltaban a la tabla de information y se termina de corregir un error en las vistas de directorio y list que impedia el uso correcto del pagiinardor en el grid

---
+++ db/migrate/20160216174236_add_field11_to_information.rb
@@ -0,0 +1,13 @@
+class AddField11ToInformation < ActiveRecord::Migration
+  def change
+    add_column :information, :field12, :string
+    add_column :information, :field13, :string
+    add_column :information, :field14, :string
+    add_column :information, :field15, :string
+    add_column :information, :field16, :string
+    add_column :information, :field17, :string
+    add_column :information, :field18, :string
+    add_column :information, :field19, :string
+    add_column :information, :field20, :string
+  end
+end
