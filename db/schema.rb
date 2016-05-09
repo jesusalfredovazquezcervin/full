53acf9800316aa2c5675b733a5ad849b0d00bbc1
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-05-08 23:34:11 -0500

    --se modifica el tipo de dato para el cmapo detail del modelo step porque a veces el texto debe ser un poco mas largo de 255

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421183042) do
+ActiveRecord::Schema.define(version: 20160509041534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -826,7 +826,7 @@ ActiveRecord::Schema.define(version: 20160421183042) do
   create_table "steps", force: true do |t|
     t.integer  "procedure_id"
     t.string   "name"
-    t.string   "detail"
+    t.text     "detail"
     t.integer  "number"
     t.datetime "created_at"
     t.datetime "updated_at"
