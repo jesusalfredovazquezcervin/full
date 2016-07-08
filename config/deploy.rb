af9b20f2427aff2b14bcaf82e179428d30c9e089
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-07-08 14:56:40 -0500

    --new version

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sys
 
 namespace :database do
   desc "Correction of sequences id"
-  task correction_seq_id: :environment do
+  task :correction_seq_id do
     ActiveRecord::Base.connection.tables.each do |t|
       ActiveRecord::Base.connection.reset_pk_sequence!(t)
     end
