930d50717255bb1fb3b561051d5bac59f51261ca
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-07-08 13:40:43 -0500

    --se modifica para que se pueda usar rails c

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :deploy_to, '/home/deploy/full'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 
 namespace :deploy do
