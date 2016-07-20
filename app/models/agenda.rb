a6bc03341e271285e112df4d11af5dd13864376f
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-07-20 10:25:57 -0500

    --se quita obligatorio el campo rfc

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,6 +1,6 @@
 class Agenda < ActiveRecord::Base
   belongs_to :cliente
-  validates :cliente_id, :nombre, :rfc, :calle, :numero, :colonia, :delegacion, :estado, :cp,
+  validates :cliente_id, :nombre, :calle, :numero, :colonia, :delegacion, :estado, :cp,
             presence: true
   validates :email,
             uniqueness: true, allow_blank: true
