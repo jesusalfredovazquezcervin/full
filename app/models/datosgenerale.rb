2be19f0be4aa0bf6d180a29bf08335d08f86239d
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-07-28 09:56:14 -0500

    --se modifican las relaciones debido a la nueva filosofia de account

--- app/models/datosgenerale.rb
+++ app/models/datosgenerale.rb
@@ -1,10 +1,14 @@
 class Datosgenerale < ActiveRecord::Base
   belongs_to :cliente
-  has_many :contact_accounts
+  has_many :contact_accounts, :dependent => :destroy
   has_many :contactos, :through => :contact_accounts
+  has_many :procedures, :dependent => :destroy
   validates :cliente_id, :horario_id, :alta, :clavesalida, :locacion, :did, :frase,
             presence: true
   validates :cliente_id,
             uniqueness:  {scope: [:account]}
   PERIODICITY = %w[diario semanal quincenal mensual bimestral trimestral semestral anual]
+
+  # Si la cuenta se elimina entonces todo lo realcionado a ella debe eliminarse, como procedimientos, relaciones entre contactos y cuentas, NO los contactos.
+
 end
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,5 +1,6 @@
 class Procedure < ActiveRecord::Base
   belongs_to :cliente
+  belongs_to :datosgenerale
   has_many :steps, :dependent => :destroy
   has_one :form
   validates :cliente_id, :name,
