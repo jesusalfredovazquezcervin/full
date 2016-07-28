c3af7f826a400e9396afd3768761385578b0a963
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-07-28 09:30:04 -0500

    --se modifica MVC por el nuevo campo de account y porque se cambia el concepto de datos grales a Account

--- app/models/datosgenerale.rb
+++ app/models/datosgenerale.rb
@@ -1,8 +1,10 @@
 class Datosgenerale < ActiveRecord::Base
+  belongs_to :cliente
+  has_many :contact_accounts
+  has_many :contactos, :through => :contact_accounts
   validates :cliente_id, :horario_id, :alta, :clavesalida, :locacion, :did, :frase,
-            presence: true,
-            uniqueness: true
+            presence: true
   validates :cliente_id,
-            uniqueness: true
+            uniqueness:  {scope: [:account]}
   PERIODICITY = %w[diario semanal quincenal mensual bimestral trimestral semestral anual]
 end
