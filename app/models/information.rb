9069e43272efd39979a613e3f8100191f4e675cf
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-08-30 09:15:11 -0500

    --se crea un scope para que solo nos brinde los registros entre dos fechas

--- app/models/information.rb
+++ app/models/information.rb
@@ -5,4 +5,7 @@ class Information < ActiveRecord::Base
   belongs_to :datosgenerale
   validates :form_id, :usuario_id,
             presence: true
+
+  scope :created_between, lambda {|start_date, end_date| where("created_at >= ? AND created_at <= ?", start_date, end_date )}
+
 end
