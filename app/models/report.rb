e84bfd4f8ea5386d3aff784288340875ede5d815
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-08-30 14:08:07 -0500

    se ree-escribe porque truena en prod

--- app/models/report.rb
+++ app/models/report.rb
@@ -15,5 +15,5 @@ class Report < ActiveRecord::Base
   validates :cliente_id,
             uniqueness:  {scope: [ :name]}
 
-  scope :activos, -> { where (active: true)}
+  scope :activos, where(:active => true)
 end
