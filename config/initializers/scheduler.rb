ccbd3387e40be68483adb8812d80dfa8d36f8c92
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2017-02-22 16:53:17 -0600

    --se ajusta para prod

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -58,7 +58,7 @@ end
 #v.every 1.day, :first_at => Time.new(2016, 9, 1, 19,10)  do # 2 pm
 #v.cron '10 21 * * *' do
 
-v.every 1.day, :first_at => Time.new(2017, 2, 22, 15)  do # 3 pm#
+v.every 1.day, :first_at => Time.new(2017, 2, 23, 15)  do # 3 pm#
 #v.every 1.day, :first_at => vesp  do
   #InformationMailer.send_report("jesuscervin@icloud.com, jesusalfredovazquezcervin@gmail.com",Report.first.id).deliver
   loop_reports("diario", "vespertino")
