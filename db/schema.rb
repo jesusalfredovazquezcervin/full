0744ca63f507f8b9b7c55ef560df21b0bd78255f
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2017-05-08 14:29:02 -0500

    --se agrega el campo avatar a usuario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503214147) do
+ActiveRecord::Schema.define(version: 20170504150149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1033,6 +1033,10 @@ ActiveRecord::Schema.define(version: 20170503214147) do
     t.boolean  "activo",                             default: true
     t.string   "role",                   limit: 255
     t.integer  "contacto_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "usuarios", ["contacto_id"], name: "index_usuarios_on_contacto_id", using: :btree
