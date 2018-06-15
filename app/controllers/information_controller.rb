1241c69d6e0dd235fa823df311b5b6bb503425a1
Jesus Alfredo Vazquez Cervin <jesuscervin@icloud.com>
2018-06-15 16:41:32 -0500

    feat(information_params): se agregan los 30 campos para que se puedan insertar

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -159,7 +159,7 @@ class InformationController < ApplicationController
         params[:information][:datosgenerale_id] = params[:cuenta_id]
         params[:information][:usuario_id] = current_user.id
       end
-      params.require(:information).permit(:form_id, :usuario_id, :field1, :field2, :field3, :field4, :field5, :field6, :field7, :field8, :field9, :field10, :field11, :field12, :field13, :field14, :field15, :field16, :field17, :field18, :field19, :field20, :datosgenerale_id, :userupdate_id)
+      params.require(:information).permit(:form_id, :usuario_id, :field1, :field2, :field3, :field4, :field5, :field6, :field7, :field8, :field9, :field10, :field11, :field12, :field13, :field14, :field15, :field16, :field17, :field18, :field19, :field20, :datosgenerale_id, :userupdate_id, :field21, :field22, :field23, :field24, :field25, :field26, :field27, :field28, :field29, :field30, :field31, :field32, :field33, :field34, :field35, :field36, :field37, :field38, :field39, :field40, :field41, :field42, :field43, :field44, :field45, :field46, :field47, :field48, :field49, :field50)
     end
 
     def dashboard
