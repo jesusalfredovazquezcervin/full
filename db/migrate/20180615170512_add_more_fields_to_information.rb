aa78d9575c3f85bb836edc6a7a3154506a08a5dd
Jesus Alfredo Vazquez Cervin <jesuscervin@icloud.com>
2018-06-15 16:38:47 -0500

    feat(fld): se agregan script que agrega 30 campos a la tabla information

---
+++ db/migrate/20180615170512_add_more_fields_to_information.rb
@@ -0,0 +1,36 @@
+class AddMoreFieldsToInformation < ActiveRecord::Migration
+  def change
+    add_column :information, :field21, :text
+    add_column :information, :field22, :text
+    add_column :information, :field23, :text
+    add_column :information, :field24, :text
+    add_column :information, :field25, :text
+    add_column :information, :field26, :text
+    add_column :information, :field27, :text
+    add_column :information, :field28, :text
+    add_column :information, :field29, :text
+    add_column :information, :field30, :text
+
+    add_column :information, :field31, :text
+    add_column :information, :field32, :text
+    add_column :information, :field33, :text
+    add_column :information, :field34, :text
+    add_column :information, :field35, :text
+    add_column :information, :field36, :text
+    add_column :information, :field37, :text
+    add_column :information, :field38, :text
+    add_column :information, :field39, :text
+    add_column :information, :field40, :text
+
+    add_column :information, :field41, :text
+    add_column :information, :field42, :text
+    add_column :information, :field43, :text
+    add_column :information, :field44, :text
+    add_column :information, :field45, :text
+    add_column :information, :field46, :text
+    add_column :information, :field47, :text
+    add_column :information, :field48, :text
+    add_column :information, :field49, :text
+    add_column :information, :field50, :text
+  end
+end
